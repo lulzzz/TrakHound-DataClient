f443f728d5e914766df6939b9e80677155859159
Patrick Ritchie <Patrick@TrakHound.com>
2017-02-15 14:52:37 -0500

    Added SystemTray application and reorganized Repo

---
+++ src/DataClient-SystemTrayApplication/Program.cs
@@ -0,0 +1,25 @@
+// Copyright (c) 2017 TrakHound Inc., All Rights Reserved.
+
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE', which is part of this source code package.
+
+using System;
+using System.Windows.Forms;
+
+namespace TrakHound.DataClient.SystemTray
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            // Instead of running a form, we run an ApplicationContext.
+            Application.Run(new TaskTrayApplicationContext());
+        }
+    }
+}
